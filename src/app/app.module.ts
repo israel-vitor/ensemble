64a29d3c774c2e150661bf446d134824d86ba449
hiltonoliveir4 <hilton.segundo12@gmail.com>
2022-03-02 13:42:45 -0300

    FEAT(TELAS) GRUPO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,27 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { ServicosComponent } from './components/servicos/servicos.component';
+import { GrupoComponent } from './components/grupo/grupo.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignUpComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    HomeComponent,
+    PerfilComponent,
+    ServicosComponent,
+    GrupoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/perfil/perfil.component.ts
+++ src/app/components/perfil/perfil.component.ts
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
+  public isEdit: boolean = true;
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
